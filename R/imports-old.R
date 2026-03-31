3d3c3840ce978178b6b59489789da7dcf52a771a
Alfonso Sanchez-Penalver <alfonso@Alfonsos-MacBook-Pro.local>
2026-03-30 20:09:17 -0400

    Added functionality about constraints, and redocumented several functions with better roxygen.

---
+++ R/imports-old.R
@@ -0,0 +1,24 @@
+#' @importFrom cli cli_abort cli_warn cli_alert_info
+#' @importFrom hardhat mold forge default_formula_blueprint
+#' @importFrom maxLik maxLik
+#' @importFrom rlang arg_match enquo eval_tidy
+#' @importFrom tibble tibble
+
+#' @importFrom stats coef
+#' @importFrom stats complete.cases
+#' @importFrom stats cor
+#' @importFrom stats cov
+#' @importFrom stats cov2cor
+#' @importFrom stats dnorm
+#' @importFrom stats formula
+#' @importFrom stats lm
+#' @importFrom stats logLik
+#' @importFrom stats pchisq
+#' @importFrom stats pnorm
+#' @importFrom stats predict
+#' @importFrom stats printCoefmat
+#' @importFrom stats update
+#' @importFrom stats update.formula
+#' @importFrom stats var
+#' @importFrom stats vcov
+NULL
